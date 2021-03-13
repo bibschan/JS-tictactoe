9de01936a13e5c4c21199536fb85eab773d1322e
Bibiana Souza <souzabibiana@hotmail.com>
2021-03-12 16:00:02 -0800

    changed icons to dog and cat

--- tictactoe.js
+++ tictactoe.js
@@ -4,16 +4,30 @@ class Board {
     activePlayer;
 
     constructor(){
-       this.board = [[0, 0, 0],
-                     [0, 0, 0],
-                     [0, 0, 0]];
-       this.isBoardFull = false;
-       this.activePlayer = 'x';
+        this.board = [[0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]];
+        this.isBoardFull = false;
+        this.activePlayer = 'x';
     }
 
     setBoardStateToFull(){
         this.isBoardFull = true;
         return isBoardFull;
+    }
+
+    resetBoard(){
+        this.board = [[0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]];
+
+        for (let index = 0; index < 3; index++){
+            for(let j = 0; j < 3; j++){
+                let attachPlayer = document.getElementById(index + '' + j);
+                attachPlayer.classList.remove('x');
+                attachPlayer.classList.remove('o');
+            }
+        }
     }
 
     checkBoardFull(){
@@ -22,17 +36,15 @@ class Board {
                 return false;
             }
         }
-        setBoardStateToFull();
+        this.setBoardStateToFull();
         return true;
     }
     
     getBoardState(){
-      return this.isBoardFull;
+        return this.isBoardFull;
     }
 
-    switchPlayer(){
-        this.activePlayer === 'x' ? this.activePlayer = 'o' : this.activePlayer = 'x';
-    }
+    
 
     checkForWin(row, column){
         let count = 0;
@@ -63,6 +75,10 @@ class Board {
         return false;
     }
 
+    switchPlayer(){
+        this.activePlayer === 'x' ? this.activePlayer = 'o' : this.activePlayer = 'x';
+    }
+
     setBoxState(row, column){
         this.checkBoardFull();
 
@@ -71,40 +87,38 @@ class Board {
             
         } else {
             alert('this box is not available')
-        }
-        
+        }  
     }
     
     getBoxState(row, column){
-      return this.board[row][column];
+        return this.board[row][column];
     }
-  }
-
-  // adding event listeners
-  let box = document.getElementsByTagName('td')
-
-for (let index = 0; index < box.length; index++) {
-    box[index].addEventListener('click', () => play(box[index].id))
 }
 
-let game = new Board();
+  // adding event listeners
+    let box = document.getElementsByTagName('td')
 
-function play(id){
-    let row, column;
-    row = id.substring(0,1);
-    column = id.substring(1,2);
-
-    if(game.getBoxState(row, column) === 0){
-        game.setBoxState(row, column)
-        let attachPlayer = document.getElementById(id);
-        attachPlayer.classList.add(game.activePlayer);
-
-        if(game.checkForWin(row, column)){
-           alert('player ' + game.activePlayer + ' won!')
-        }
-    } else {
-        alert('this box is not available')
+    for (let index = 0; index < box.length; index++) {
+        box[index].addEventListener('click', () => play(box[index].id))
     }
 
-    console.log(game.board)
+    let game = new Board();
+
+    function play(id){
+        let row, column;
+        row = id.substring(0,1);
+        column = id.substring(1,2);
+
+        if(game.getBoxState(row, column) === 0){
+            game.setBoxState(row, column);
+            let attachPlayer = document.getElementById(id);
+            attachPlayer.classList.add(game.activePlayer);
+
+            if(game.checkForWin(row, column)){
+                alert((game.activePlayer === 'x' ? 'Cool Cat' : 'Chilling Doge') + ' won!')
+                game.resetBoard();
+            }
+        } else {
+            alert('this box is not available')
+        }
 }
